f2dc17b9915a8ede0ccb32677d13fe33355b82da
mahbubme <mail@mahbub.me>
2016-11-09 20:04:13 +0600

    delete slide image permanantly

--- src/resources/templates/back/delete_slide.php
+++ src/resources/templates/back/delete_slide.php
@@ -1,7 +1,16 @@
 <?php
 
     if (isset($_GET['id'])) {
-        $query = query("DELETE FROM slides WHERE slide_id = ". escape_string($_GET['id']) ." ");
+
+    	$query_find_image = query("SELECT slide_image FROM slides WHERE slide_id = " . escape_string($_GET['id']) . " ");
+    	confirm($query_find_image);
+
+    	$row = fetch_array($query_find_image);
+    	$target_path = UPLOAD_DIRECTORY . DS . $row['slide_image'];
+
+    	unlink($target_path);
+
+		$query = query("DELETE FROM slides WHERE slide_id = ". escape_string($_GET['id']) ." ");
         confirm($query);
 
         set_message("<div class='alert alert-danger text-center' role='alert'>Slide Deleted</div>");
